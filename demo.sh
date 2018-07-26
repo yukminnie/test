96a14a18a8a973ed87aef5cc1e6856a7faa33cf7
yukminnie <dadiaomin@126.com>
2018-07-26 19:29:28 +0800

    第一个脚本

---
+++ demo.sh
@@ -0,0 +1,20 @@
+#!/bin bash
+if [ -d $1 ]; then
+    echo 'dir exists!'
+    exit 
+else
+    mkdir $1
+    cd $1
+    mkdir css js
+    touch index.html 
+    echo '<!DOCTYPE>\n<title>Hello</title>\n<h1>Hi</h1>' > index.html
+    
+    touch css/style.css
+    echo 'h1{color: red;}' > css/style.css
+
+    touch js/main.js
+    echo 'var string = "Hello World"\n alert(string)' > js/main.js
+    
+    echo 'success'
+    exit
+fi
